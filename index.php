c177c49fb9ea76c8278dfb1a4c0c4bddc71605d1
zachary derby <zachd29@hotmail.com>
2018-11-01 10:54:10 -0400

    I added new files

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>this is my first webpage</title>
+<meta name="description" content="Free Web tutorials">
+<meta name="keywords" content="HTML, CSS, XML, JavaScript">
+<meta http-equiv="refresh" content="30">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="css/styles.css">
+
+
+</head>
+<body>
+  <?php
+
+  echo 'Hello, World this is PHP';
+
+  ?>
+
+<button>Click me</button>	
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+  <li>Creamers</li>
+  <li>Flavors</li>
+  <li>strengths</li>
+  <li>beer</li>
+  <li>wine</li>
+  <li>liquor</li>
+</ul>
+
+<h2 style="color:purple;">Links and Videos</h2>
+<p>Mommy's Favorite Websites.</p>
+<a href="https://www.amazon.com" target="_blank">This is a link</a>
+<br><br>
+<a href="https://www.target.com" target="_blank">
+This is another link</a>
+<br><br><img src="images/mountain2.jpg" style="width:300px;">
+
+<table class="thisone">
+
+<tr>
+<td>one</td>
+
+<td>two</td>
+
+<td>three</td>
+</tr>
+<tr>
+<td>one</td>
+
+<td>two</td>
+
+<td>three</td>
+</tr>
+<tr>
+<td>one</td>
+
+<td>two</td>
+
+<td>three</td>
+</tr>
+<tr>
+<td>one</td>
+
+<td>two</td>
+
+<td>three</td>
+</tr>
+</table>
+<br><br>
+<table class="thisone" >
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+<br><br>&nbsp;&nbsp;&nbsp;
+<table class="thisone">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th rowspan="2">Telephone:</th>
+    <td>55577854</td>
+  </tr>
+  <tr>
+    <td>55577855</td>
+  </tr>
+
+</table>
+<br><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/aEzZLXBH3rU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br><br>
+<iframe width="300" height="100" src="https://www.youtube.com/embed/31SCjwyIMl8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Ul-WtsPH8s0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br><br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/HhVToYE5dWw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<br><br>
+<br><br>
+<br><br>
+
+THIS VIDEO IS FOR CARMELLA MY LIL PENGUIN!!
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Sb_PZndWk4k" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</body>
+</html>
